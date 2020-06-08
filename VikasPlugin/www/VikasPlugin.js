f3788bec95886c99d3c691d40b26f974db37f367
Vikas Krishnan <vikas@qburst.com>
2020-06-08 13:20:38 +0530

    Plugin files

---
+++ VikasPlugin/www/VikasPlugin.js
@@ -0,0 +1,25 @@
+var exec = require('cordova/exec');
+
+// exports.coolMethod = function (arg0, success, error) {
+//     exec(success, error, 'VikasPlugin', 'coolMethod', [arg0]);
+// };
+
+var PLUGIN_NAME = "VikasPlugin";
+var VikasPlugin = function() {};
+VikasPlugin.echo = function(arg0, onSuccess, onError) {
+	// body...
+	exec(onSuccess, onError, "VikasPlugin", "echo", [arg0]);
+};
+
+// exports.echo = function(arg0, success, error) {
+//   exec(success, error, "VikasPlugin", "echo", [arg0]);
+// };
+
+// exports.echojs = function(arg0, success, error) {
+//   if (arg0 && typeof(arg0) === 'string' && arg0.length > 0) {
+//     success(arg0);
+//   } else {
+//     error('Empty message!');
+//   }
+// };
+module.exports = VikasPlugin;
